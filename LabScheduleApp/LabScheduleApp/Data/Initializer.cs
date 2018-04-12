82ed9b83ddcd4e24e8a659fc9f687aed6cf3c9b4
IulianIonescu <iulius_stefi@yahoo.com>
2018-04-12 14:06:34 +0300

    Replace initial Project
    The initial project skeleton was replace with the project which contains the Login and Register options

---
+++ LabScheduleApp/LabScheduleApp/Data/Initializer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LabScheduleApp.Data
+{
+    public static class Initializer
+    {
+        public static async Task initial(RoleManager<IdentityRole> roleManager)
+        {
+            if(!await roleManager.RoleExistsAsync("Admin"))
+            {
+                var users = new IdentityRole("Admin");
+                await roleManager.CreateAsync(users);
+            }
+            if (!await roleManager.RoleExistsAsync("Teacher"))
+            {
+                var users = new IdentityRole("Teacher");
+                await roleManager.CreateAsync(users);
+            }
+            if (!await roleManager.RoleExistsAsync("Student"))
+            {
+                var users = new IdentityRole("Student");
+                await roleManager.CreateAsync(users);
+            }
+        }
+    }
+}
--- LabScheduleApp/LabScheduleApp/Startup.cs
+++ LabScheduleApp/LabScheduleApp/Startup.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using LabScheduleApp.Data;
+using LabScheduleApp.Models;
+using LabScheduleApp.Services;
+using Microsoft.Extensions.Logging;
 
 namespace LabScheduleApp
 {
@@ -21,16 +27,28 @@ namespace LabScheduleApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }
             else
             {
@@ -39,12 +57,15 @@ namespace LabScheduleApp
 
             app.UseStaticFiles();
 
+            app.UseAuthentication();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
         }
     }
 }

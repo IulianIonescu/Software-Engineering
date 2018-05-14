8f50a9497f04b2baaae558b28e34ff1b1c172e62
Radu Dinu <radurmx@gmail.com>
2018-05-14 22:27:03 +0300

    Add students grid & modify home pages

---
+++ WebApplication1/WebApplication1/Controllers/LaboratoriesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IdentitySample.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize(Roles = "Student")]
+    public class LaboratoriesController : Controller
+    {
+        public LaboratoriesController()
+        {
+        }
+
+        public LaboratoriesController(ApplicationUserManager userManager, ApplicationRoleManager roleManager)
+        {
+            UserManager = userManager;
+            RoleManager = roleManager;
+        }
+
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        private ApplicationRoleManager _roleManager;
+        public ApplicationRoleManager RoleManager
+        {
+            get
+            {
+                return _roleManager ?? HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
+            }
+            private set
+            {
+                _roleManager = value;
+            }
+        }
+    }

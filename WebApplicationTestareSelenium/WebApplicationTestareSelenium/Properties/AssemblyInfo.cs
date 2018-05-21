425a19f7ed47fe89a3d3e960bec8027aa616ab84
IulianIonescu <iulius_stefi@yahoo.com>
2018-05-21 22:15:38 +0300

    Add Test Project

---
+++ WebApplicationTestareSelenium/WebApplicationTestareSelenium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebApplicationTestareSelenium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApplicationTestareSelenium")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("68c00ba0-bb2d-4e7a-afa5-13724c2412b3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebApplicationTestareSelenium/WebApplicationTestareSelenium/UnitTest1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebApplicationTestareSelenium
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AdminLogin()
+        {
+            ChromeDriver driver = new ChromeDriver();
+            try
+            {
+                string url = "http://localhost:54175/Account/Login";
+                driver = new ChromeDriver();
+                driver.Navigate().GoToUrl(url);
+                driver.Manage().Window.Maximize();
+                driver.FindElement(By.Id("Email")).SendKeys("admin@example.com");
+                driver.FindElement(By.Id("Password")).SendKeys("Admin@123456");
+                driver.FindElement(By.Id("btnLogin")).Click();
+                WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 20));
+                wait.Until(wt => wt.FindElement(By.Id("logoutForm")));
+                driver.Close();
+                driver.Dispose();
+            }
+            catch
+            {
+                ITakesScreenshot screenshotDriver = driver as ITakesScreenshot;
+                Screenshot screenshot = screenshotDriver.GetScreenshot();
+                screenshot.SaveAsFile("E:/GitHub/Software-Engineering/WebApplication/test.png",ScreenshotImageFormat.Png);
+                driver.Quit();
+            }
+        }
+
+        [TestMethod]
+        public void TeacherLogin()
+        {
+            ChromeDriver driver = new ChromeDriver();
+            try
+            {
+                string url = "http://localhost:54175/Account/Login";
+                driver = new ChromeDriver();
+                driver.Navigate().GoToUrl(url);
+                driver.Manage().Window.Maximize();
+                driver.FindElement(By.Id("Email")).SendKeys("profu@exemplu.com");
+                driver.FindElement(By.Id("Password")).SendKeys("Parola_1");
+                driver.FindElement(By.Id("btnLogin")).Click();
+                WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 20));
+                wait.Until(wt => wt.FindElement(By.Id("logoutForm")));
+                driver.Close();
+                driver.Dispose();
+            }
+            catch
+            {
+                ITakesScreenshot screenshotDriver = driver as ITakesScreenshot;
+                Screenshot screenshot = screenshotDriver.GetScreenshot();
+                screenshot.SaveAsFile("E:/GitHub/Software-Engineering/WebApplication1/test.png", ScreenshotImageFormat.Png);
+                driver.Quit();
+            }
+        }
+
+        [TestMethod]
+        public void StudentLogin()
+        {
+            ChromeDriver driver = new ChromeDriver();
+            try
+            {
+                string url = "http://localhost:54175/Account/Login";
+                driver = new ChromeDriver();
+                driver.Navigate().GoToUrl(url);
+                driver.Manage().Window.Maximize();
+                driver.FindElement(By.Id("Email")).SendKeys("student@yahoo.com");
+                driver.FindElement(By.Id("Password")).SendKeys("Parola_1");
+                driver.FindElement(By.Id("btnLogin")).Click();
+                WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
+                wait.Until(wt => wt.FindElement(By.Id("logoutForm")));
+                driver.Close();
+                driver.Dispose();
+            }
+            catch
+            {
+                ITakesScreenshot screenshotDriver = driver as ITakesScreenshot;
+                Screenshot screenshot = screenshotDriver.GetScreenshot();
+                screenshot.SaveAsFile("E:/GitHub/Software-Engineering/WebApplication1/test.png", ScreenshotImageFormat.Png);
+                driver.Quit();
+            }
+        }
+        [TestMethod]
+        public void StudentLoginWrongPassword()
+        {
+            ChromeDriver driver = new ChromeDriver();
+            try
+            {
+                string url = "http://localhost:54175/Account/Login";
+                driver = new ChromeDriver();
+                driver.Navigate().GoToUrl(url);
+                driver.Manage().Window.Maximize();
+                driver.FindElement(By.Id("Email")).SendKeys("student@yahoo.com");
+                driver.FindElement(By.Id("Password")).SendKeys("ParolaIncorecta");
+                driver.FindElement(By.Id("btnLogin")).Click();
+                WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 20));
+                wait.Until(wt => wt.FindElement(By.ClassName("validation-summary-errors text-danger")));
+                driver.Close();
+                driver.Dispose();
+            }
+            catch
+            {
+                ITakesScreenshot screenshotDriver = driver as ITakesScreenshot;
+                Screenshot screenshot = screenshotDriver.GetScreenshot();
+                screenshot.SaveAsFile("E:/GitHub/Software-Engineering/WebApplication1/test.png", ScreenshotImageFormat.Png);
+                driver.Quit();
+            }
+        }
+
+        [TestMethod]
+        public void LaunchForgotPasswordPage()
+        {
+            ChromeDriver driver = new ChromeDriver();
+            try
+            {
+                string url = "http://localhost:54175/Account/Login";
+                driver = new ChromeDriver();
+                driver.Navigate().GoToUrl(url);
+                driver.Manage().Window.Maximize();
+               // driver.FindElement(By.Id("ForgotPasswordClass")).Click();
+                driver.FindElement(By.PartialLinkText("Forgot your password?")).Click();
+                WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 15));
+                wait.Until(wt => wt.FindElement(By.Id("EmailLink")));
+                driver.Close();
+                driver.Dispose();
+            }
+            catch
+            {
+                ITakesScreenshot screenshotDriver = driver as ITakesScreenshot;
+                Screenshot screenshot = screenshotDriver.GetScreenshot();
+                screenshot.SaveAsFile("E:/GitHub/Software-Engineering/WebApplication1/test.png", ScreenshotImageFormat.Png);
+                driver.Quit();
+            }
+        }
+    }
+}
